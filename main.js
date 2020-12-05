d20f158478ad1721eed4626c037f681e22f7223b
Richard Higgins <richard@relh.net>
2020-12-05 13:55:56 -0500

    hover gt pred effect

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $('#ex1').slider({
 	}
 });
 
+/*
 $("#ex4").slider({
 	reversed: true,
 	tooltip: 'always',
@@ -21,3 +22,38 @@ $("#ex5").slider({
 		return 'MAE: ' + value;
 	}
 });
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var image = new Image();
+image.src = "./prediction.jpg";
+image.onload = function() {
+    ctx.drawImage(image, 256, 0);
+};
+*/
+var optionstwo = {
+    width: 512, // required
+    //img: "./inclination_pred.png",
+    // more options here
+    zoomWidth: 512,
+    offset: {
+      vertical: 0,
+      horizontal: 10
+    },
+    zoomPosition: 'right'
+};
+
+var options = {
+    width: 512, // required
+    // more options here
+    img: "./inclination_pred.png",
+    zoomWidth: 512,
+    offset: {
+      vertical: 0,
+      horizontal: 10
+    },
+    zoomPosition: 'left'
+};
+new ImageZoom(document.getElementById("img-container"), options);
+new ImageZoom(document.getElementById("img-container"), optionstwo);
