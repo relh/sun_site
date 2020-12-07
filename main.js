a58e791881179a825a7434b255ec1936c58dde98
Richard Higgins <richard@relh.net>
2020-12-07 17:46:41 -0500

    works

--- main.js
+++ main.js
@@ -5,38 +5,17 @@ $('#ex1').slider({
 	}
 });
 
-/*
-$("#ex4").slider({
-	reversed: true,
-	tooltip: 'always',
-	tooltip_position:'left',
-	formatter: function(value) {
-		return 'Max Value: ' + value;
-	}
-});
+var currentImage = 1;
+var container = document.getElementById('img-container');
+var containergt = document.getElementById('img-container-gt');
 
-$("#ex5").slider({
-	reversed: true,
-	tooltip: 'always',
-	formatter: function(value) {
-		return 'MAE: ' + value;
-	}
-});
-
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-
-var image = new Image();
-image.src = "./prediction.jpg";
-image.onload = function() {
-    ctx.drawImage(image, 256, 0);
-};
-*/
-var optionstwo = {
+var pred_options = {
     width: 512, // required
-    //img: "./inclination_pred.png",
+    height: 512,
+    img: "./full_prediction.jpg",
     // more options here
-    zoomWidth: 512,
+    //zoomWidth: 512,
+    //scale: 1,
     offset: {
       vertical: 0,
       horizontal: 10
@@ -44,16 +23,99 @@ var optionstwo = {
     zoomPosition: 'right'
 };
 
-var options = {
+var gt_options = {
     width: 512, // required
+    height: 512,
     // more options here
     img: "./inclination_pred.png",
-    zoomWidth: 512,
+    //zoomWidth: 512,
+    //scale: 1,
     offset: {
       vertical: 0,
       horizontal: 10
     },
     zoomPosition: 'left'
 };
-new ImageZoom(document.getElementById("img-container"), options);
-new ImageZoom(document.getElementById("img-container"), optionstwo);
+
+function triggerOne(e, context) {
+	var ne = new MouseEvent(e.type, e)
+	container.dispatchEvent(ne, context);
+}
+
+function triggerTwo(e, context) {
+	var ne = new MouseEvent(e.type, e)
+	containergt.dispatchEvent(ne, context);
+}
+
+function make_zoom_one() {
+        window.ground_truth_zoom = new ImageZoom(container, gt_options);
+        var all_img = container.getElementsByTagName('img');
+        all_img[0].className = "image1";
+        all_img[0].id = "top-image";
+        all_img[0].style = "width: 512px; height: 512px;";
+        //all_img[1].style = "margin-left: -512px; width: 512px; height:512px;";
+
+}
+
+function make_zoom_two() {
+        window.prediction_zoom = new ImageZoom(containergt, pred_options);
+        var all_img = containergt.getElementsByTagName('img');
+        all_img[0].className = "image2";
+        all_img[0].style = "width: 512px; height: 512px;";
+}
+window.prediction_zoom = new ImageZoom(container, pred_options);
+window.ground_truth_zoom = new ImageZoom(containergt, gt_options);
+//make_zoom_one();
+//make_zoom_two();
+
+//window['prediction_zoom'].kill();
+//window['ground_truth_zoom'].kill();
+//make_zoom_one();
+//make_zoom_two();
+
+$('#ex1').slider().on('slideStop', function(value) {
+        console.log(value);
+
+        window['prediction_zoom'].kill();
+        window['ground_truth_zoom'].kill();
+
+        pred_options.img = './inclination_pred.png';
+        gt_options.img = './inclination_pred.png';
+
+        window.prediction_zoom = new ImageZoom(container, pred_options);
+        window.ground_truth_zoom = new ImageZoom(containergt, gt_options);
+        //make_zoom_one();
+        //make_zoom_two();
+
+
+        var container_img = container.getElementsByTagName('img');
+        var container_gt_img = containergt.getElementsByTagName('img');
+        container_img[0].style.width = 512;
+        container_img[0].style.height = 512;
+
+        container_gt_img[0].style.width = 512;
+        container_gt_img[0].style.height = 512;
+        //all_img[0].src = './inclination_pred.png';
+        //all_img[1].src = './inclination_pred.png';
+        //all_img[1].parentNode.removeChild(all_img[0]);
+        //all_img[1].parentNode.removeChild(all_img[0]);
+
+        //var all_div = container.getElementsByTagName('div');
+        //var one_old_style = all_div[1].style
+        //var three_old_style = all_div[3].style
+
+        //all_div[1].style.backgroundImage = 'url("file:///home/relh/Code/sun_site/inclination_pred.png");'
+        //all_div[3].style.backgroundImage = 'url("file:///home/relh/Code/sun_site/inclination_pred.png");'
+
+        //console.log(all_img);
+        //window.ground_truth_zoom = new ImageZoom(container, gt_options);
+        //window.prediction_zoom = new ImageZoom(container, pred_options);
+
+        //$('#top-image').attr("src", "./inclination_pred.png");
+        //var all_img = container.getElementsByTagName('img');
+        //all_img[0].id = "top-image";
+        //all_img[0].style = "width: 512px; height: 512px;";
+        //all_img[1].style = "margin-left: -512px; width: 512px; height:512px;";
+        //all_img[0].className = "image1";
+        //all_img[1].className = "image2";
+});
---
+++ make_1024s.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pdb
+import os
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+gts = '/home/relh/Code/sun_site/ground_truths/'
+preds = '/home/relh/Code/sun_site/predictions/'
+outputs = '/home/relh/Code/sun_site/images/'
+
+print(os.listdir(gts + 'yearputs/'))
+
+for target in ['field', 'inclination', 'azimuth', 'vlos_mag', 'eta_0', 'dop_width', 'src_continuum', 'src_grad']:
+#for target in ['vlos_mag', 'eta_0', 'dop_width', 'src_continuum', 'src_grad']:
+
+    min_limit = 0.0 
+    image_colormap = plt.get_cmap('viridis')
+    if 'azimuth' in target:
+        image_colormap = plt.get_cmap('twilight')
+        max_limit = 180.0
+    elif 'inclination' in target:
+        image_colormap = plt.get_cmap('Spectral')
+        max_limit = 180.0
+    elif 'field' in target or 'b_fill_factor' in target:
+        image_colormap = plt.get_cmap('plasma')
+        max_limit = 5000.0
+    elif 'vlos_mag' in target:
+        image_colormap = plt.get_cmap('coolwarm')
+        min_limit = -700000.0
+        max_limit = 700000.0
+    elif 'eta_0' in target:
+        image_colormap = plt.get_cmap('cividis')
+        max_limit = 50.0
+    elif 'dop_width' in target:
+        image_colormap = plt.get_cmap('BuPu')
+        max_limit = 60.0
+    elif 'src_continuum' in target:
+        image_colormap = plt.get_cmap('Greens')
+        max_limit = 29060.61
+    elif 'src_grad' in target:
+        image_colormap = plt.get_cmap('Purples')
+        max_limit = 52695.32
+    image_colormap.set_bad('black', 1.)
+
+    for setting in ['yearputs']: #, 'monthputs']:
+        gt_files = sorted(os.listdir(gts + setting + '/' + target + '/'))
+        pred_files = sorted(os.listdir(preds + setting + '/' + target + '/'))
+        field_files = sorted(os.listdir(gts + setting + '/' + 'field' + '/'))
+
+        '''
+        for gt_file, pred in zip(gt_files, pred_files):
+            date = pred.split('_')[0] + '_' + pred.split('_')[1]
+            new_name = gts + setting + '/' + target + '/' + date + '_groundtruth.pkl'
+            os.rename(gts + setting + '/' + target + '/' + gt_file, new_name)
+            print(new_name)
+        '''
+
+        for i, pred in enumerate(pred_files):
+            pred_disk_tensor = torch.zeros((4096, 4096), requires_grad=False, device='cpu')
+            gt_disk_tensor = torch.zeros((4096, 4096), requires_grad=False, device='cpu')
+            field_disk_tensor = torch.zeros((4096, 4096), requires_grad=False, device='cpu')
+
+            date = pred.split('_')[0] + '_' + pred.split('_')[1]
+            print(date)
+            gt = [x for x in gt_files if date in x]
+            if len(gt) == 0: continue
+            else: 
+                gt = gt[0]
+                field = [x for x in field_files if date in x][0]
+
+            #print(os.path.exists(gts + date + '_groundtruth.pkl'))
+            #print(gts + date + '_groundtruth.pkl')
+            gt_tensor = torch.load(gts + setting + '/' + target + '/' + gt)
+            pred_tensor = torch.load(preds + setting + '/' + target + '/' + pred)
+            field_tensor = torch.load(gts + setting + '/' + 'field' + '/' + gt)
+
+            for sub_i in range(16):
+                x = sub_i // 4
+                y = sub_i % 4
+                gt_disk_tensor[x*1024:(x+1)*1024, y*1024:(y+1)*1024] = gt_tensor[sub_i]
+                pred_disk_tensor[x*1024:(x+1)*1024, y*1024:(y+1)*1024] = pred_tensor[sub_i]
+                field_disk_tensor[x*1024:(x+1)*1024, y*1024:(y+1)*1024] = field_tensor[sub_i]
+
+            gt_disk_tensor[field_disk_tensor <= 0.7] = np.nan
+            pred_disk_tensor[field_disk_tensor <= 0.7] = np.nan
+
+            plt.imsave(outputs + setting + '/' + target + '/' + date + '_gt_image.jpg', gt_disk_tensor, cmap=image_colormap, vmin=min_limit, vmax=max_limit)#, vmin=-maxmin, vmax=maxmin)
+            plt.imsave(outputs + setting + '/' + target + '/' + date + '_pred_image.jpg', pred_disk_tensor, cmap=image_colormap, vmin=min_limit, vmax=max_limit)#, vmin=-maxmin, vmax=maxmin)
+
+            if i == 10: break
